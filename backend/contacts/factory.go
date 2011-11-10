a09e4e728ce0a48f255326483483de9cc881f0cc
Aalok Shah <aalok@pomack.com>
2011-11-10 13:55:22 -0500

    Add new ContactsService factory methods and add Facebook support to Makefile

---
+++ backend/contacts/factory.go
@@ -0,0 +1,51 @@
+package contacts
+
+var (
+   contactServicesMap map[string]ContactsService
+   selfUpdateableContactServices []ContactsService
+   updateableContactServices []ContactsService
+)
+
+func init() {
+    contactServicesMap = make(map[string]ContactsService) 
+    for _, service := range []ContactsService{
+        NewFacebookContactService(),
+        NewGoogleContactService(),
+        NewGooglePlusContactService(),
+        NewLinkedInContactService(),
+        NewSmugMugContactService(),
+        NewTwitterContactService(),
+        NewYahooContactService(),
+    } {
+        contactServicesMap[service.ServiceId()] = service
+    }
+    l := len(contactServicesMap)
+    selfUpdateableContactServices := make([]ContactsService, l, l)
+    updatedableContactServices := make([]ContactsService, l, l)
+    i, j := 0, 0
+    for _, service := range contactServicesMap {
+        if service.CanUpdateGroup(true) || service.CanUpdateContact(true) {
+            selfUpdateableContactServices[i] = service
+            i++
+        }
+        if service.CanUpdateGroup(false) || service.CanUpdateContact(false) {
+            updateableContactServices[j] = service
+            j++
+        }
+    }
+    selfUpdateableContactServices = selfUpdateableContactServices[0:i]
+    updatedableContactServices = updatedableContactServices[0:j]
+}
+
+func ContactServiceForName(name string) ContactsService {
+    cs, _ := contactServicesMap[name]
+    return cs
+}
+
+func SelfUpdateableContactServices() []ContactsService {
+    return selfUpdateableContactServices
+}
+
+func UpdateableContactServices() []ContactsService {
+    return updateableContactServices
+}
