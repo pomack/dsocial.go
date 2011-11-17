25e1d93a6c6307e82d90917440bd6baf80b57fc4
Aalok Shah <aalok@pomack.com>
2011-11-17 17:27:50 -0500

    Add validation logic to account models

--- models/dsocial/accounts.go
+++ models/dsocial/accounts.go
@@ -1,5 +1,10 @@
 package dsocial
 
+import (
+    "os"
+    "strings"
+)
+
 type UserRole int32
 
 const (
@@ -20,6 +25,8 @@ type Consumer struct {
     AuthorizationPage string `json:"authorization_page,omitempty"`
     ShortName         string `json:"short_name,omitempty"`
     Name              string `json:"name,omitempty"`
+    Email             string `json:"email,omitempty"`
+    PhoneNumber       string `json:"phone_number,omitempty"`
     IsTrusted         bool   `json:"is_trusted,omitempty"`
     IsSuggested       bool   `json:"is_suggested,omitempty"`
     AllowLogin        bool   `json:"allow_login,omitempty"`
@@ -47,3 +54,50 @@ type ExternalUser struct {
     ExternalUserId   string `json:"external_user_id,omitempty"`
     Name             string `json:"name,omitempty"`
 }
+
+func (p *Consumer) Validate(createNew bool, errors map[string][]os.Error) (isValid bool) {
+    if errors == nil {
+        errors = make(map[string][]os.Error)
+    }
+    p.PersistableModel.Validate(createNew, errors)
+    p.DomainName, _ = validateDomainName(p.DomainName, false, "domain_name", errors)
+    p.HomePage, _ = validateUrl(p.HomePage, false, "home_page", errors)
+    p.AuthorizationPage, _ = validateUrl(p.AuthorizationPage, false, "authorization_page", errors)
+    p.ShortName, _ = validateAlphaNumeric(p.ShortName, true, false, true, false, false, "short_name", errors)
+    p.Name, _ = validateNonEmpty(p.Name, true, "name", errors)
+    p.Email, _ = validateEmail(p.Email, false, "email", errors)
+    p.PhoneNumber = strings.TrimSpace(p.PhoneNumber)
+    isValid = len(errors) == 0
+    return
+}
+
+func (p *User) Validate(createNew bool, errors map[string][]os.Error) (isValid bool) {
+    if errors == nil {
+        errors = make(map[string][]os.Error)
+    }
+    p.PersistableModel.Validate(createNew, errors)
+    p.Role = p.Role & ROLE_STANDARD & ROLE_BUSINESS_SUPPORT & ROLE_TECHNICAL_SUPPORT & ROLE_BACKUP_OPERATOR & ROLE_SECURITY_OFFICER & ROLE_OWNER & ROLE_ADMIN
+    if p.Role == 0 {
+        p.Role = ROLE_STANDARD
+    }
+    p.Username, _ = validateAlphaNumeric(p.Username, true, false, true, false, false, "short_name", errors)
+    p.Email, _ = validateEmail(p.Email, false, "email", errors)
+    p.PhoneNumber = strings.TrimSpace(p.PhoneNumber)
+    p.Address = strings.TrimSpace(p.Address)
+    p.ContactId, _ = validateId(p.ContactId, true, "contact_id", errors)
+    isValid = len(errors) == 0
+    return
+}
+
+func (p *ExternalUser) Validate(createNew bool, errors map[string][]os.Error) (isValid bool) {
+    if errors == nil {
+        errors = make(map[string][]os.Error)
+    }
+    p.PersistableModel.Validate(createNew, errors)
+    p.ConsumerId, _ = validateId(p.ConsumerId, false, "consumer_id", errors)
+    p.ExternalUserId, _ = validateId(p.ExternalUserId, false, "external_user_id", errors)
+    p.Name, _ = validateNonEmpty(p.Name, true, "name", errors)
+    isValid = len(errors) == 0
+    return
+}
+
