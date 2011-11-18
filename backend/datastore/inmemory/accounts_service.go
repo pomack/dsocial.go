425803a763184c054b9510122c1a67110b5c3041
Aalok Shah <aalok@pomack.com>
2011-11-18 01:05:41 -0500

    Fill out InMemoryDataStore for Accounts DataStore

--- backend/datastore/inmemory/accounts_service.go
+++ backend/datastore/inmemory/accounts_service.go
@@ -5,7 +5,6 @@ import (
     ba "github.com/pomack/dsocial.go/backend/accounts"
     "os"
     "strconv"
-    "time"
 )
 
 func (p *InMemoryDataStore) retrieveUserAccountCollection() (m *inMemoryCollection) {
@@ -35,17 +34,20 @@ func (p *InMemoryDataStore) generateIdForAccount(collectionName string) (string)
 }
 
 func (p *InMemoryDataStore) CreateUserAccount(user *dm.User) (*dm.User, os.Error) {
+    if user == nil {
+        return nil, nil
+    }
     if user.Id == "" {
         user.Id = p.generateIdForAccount(_INMEMORY_USER_ACCOUNT_COLLECTION_NAME)
     }
     if _, ok := p.retrieve(_INMEMORY_USER_ACCOUNT_COLLECTION_NAME, user.Id); ok {
-        return user, ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_ID
+        return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_ID
     }
     if _, ok := p.retrieve(_INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, user.Username); ok {
-        return user, ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_USERNAME
+        return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_USERNAME
     }
     if _, ok := p.retrieve(_INMEMORY_USER_ACCOUNT_ID_FOR_EMAIL_COLLECTION_NAME, user.Email); ok {
-        return user, ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_EMAIL
+        return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_EMAIL
     }
     p.store(user.Id, _INMEMORY_USER_ACCOUNT_COLLECTION_NAME, user.Id, user)
     p.store(user.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, user.Username, user.Id)
@@ -54,75 +56,297 @@ func (p *InMemoryDataStore) CreateUserAccount(user *dm.User) (*dm.User, os.Error
 }
 
 func (p *InMemoryDataStore) UpdateUserAccount(user *dm.User) (*dm.User, os.Error) {
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        return user, dm.ERR_MUST_SPECIFY_ID
+    }
+    oldUserI, _ := p.retrieve(_INMEMORY_USER_ACCOUNT_COLLECTION_NAME, user.Id)
+    var oldUser *dm.User = nil
+    if oldUserI != nil {
+        oldUser = oldUserI.(*dm.User)
+    }
+    if oldUser == nil || oldUser.Username != user.Username {
+        if _, ok := p.retrieve(_INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, user.Username); ok {
+            return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_USERNAME
+        }
+    }
+    if oldUser == nil || oldUser.Email != user.Email {
+        if _, ok := p.retrieve(_INMEMORY_USER_ACCOUNT_ID_FOR_EMAIL_COLLECTION_NAME, user.Email); ok {
+            return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_EMAIL
+        }
+    }
+    p.store(user.Id, _INMEMORY_USER_ACCOUNT_COLLECTION_NAME, user.Id, user)
+    p.store(user.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, user.Username, user.Id)
+    p.store(user.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_EMAIL_COLLECTION_NAME, user.Email, user.Id)
+    if oldUser != nil {
+        if oldUser.Username != user.Username {
+            p.delete(user.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, oldUser.Username)
+        }
+        if oldUser.Email != user.Email {
+            p.delete(user.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_EMAIL_COLLECTION_NAME, oldUser.Email)
+        }
+    }
     return nil, nil
 }
 
 func (p *InMemoryDataStore) DeleteUserAccount(user *dm.User) (*dm.User, os.Error) {
-    return nil, nil
+    if user == nil {
+        return nil, nil
+    }
+    oldUserI, _ := p.retrieve(_INMEMORY_USER_ACCOUNT_COLLECTION_NAME, user.Id)
+    var oldUser *dm.User = nil
+    if oldUserI != nil {
+        oldUser = oldUserI.(*dm.User)
+    }
+    if oldUser != nil {
+        p.delete(oldUser.Id, _INMEMORY_USER_ACCOUNT_COLLECTION_NAME, oldUser.Id)
+        p.delete(oldUser.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, oldUser.Username)
+        p.delete(oldUser.Id, _INMEMORY_USER_ACCOUNT_ID_FOR_EMAIL_COLLECTION_NAME, oldUser.Email)
+    }
+    return oldUser, nil
 }
 
-
-func (p *InMemoryDataStore) CreateConsumerAccount(user *dm.Consumer) (*dm.User, os.Error) {
-    return nil, nil
+func (p *InMemoryDataStore) CreateConsumerAccount(user *dm.Consumer) (*dm.Consumer, os.Error) {
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        user.Id = p.generateIdForAccount(_INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME)
+    }
+    if _, ok := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, user.Id); ok {
+        return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_ID
+    }
+    if _, ok := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, user.ShortName); ok {
+        return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_SHORTNAME
+    }
+    p.store(user.Id, _INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, user.Id, user)
+    p.store(user.Id, _INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, user.ShortName, user.Id)
+    p.addToStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_NAME_COLLECTION_NAME, user.Name, user.Id, user.Id)
+    p.addToStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_DOMAIN_NAME_COLLECTION_NAME, user.DomainName, user.Id, user.Id)
+    return user, nil
 }
 
-func (p *InMemoryDataStore) UpdateConsumerAccount(user *dm.Consumer) (*dm.User, os.Error) {
-    return nil, nil
+func (p *InMemoryDataStore) UpdateConsumerAccount(user *dm.Consumer) (*dm.Consumer, os.Error) {
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        return user, dm.ERR_MUST_SPECIFY_ID
+    }
+    oldUserI, _ := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, user.Id)
+    var oldUser *dm.Consumer = nil
+    if oldUserI != nil {
+        oldUser = oldUserI.(*dm.Consumer)
+    }
+    if oldUser == nil || oldUser.ShortName != user.ShortName {
+        if _, ok := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, user.ShortName); ok {
+            return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_SHORTNAME
+        }
+    }
+    if oldUser == nil || oldUser.DomainName != user.DomainName {
+        if _, ok := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_DOMAIN_NAME_COLLECTION_NAME, user.DomainName); ok {
+            return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_DOMAIN_NAME
+        }
+    }
+    p.store(user.Id, _INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, user.Id, user)
+    p.store(user.Id, _INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, user.ShortName, user.Id)
+    p.addToStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_NAME_COLLECTION_NAME, user.Name, user.Id, user.Id)
+    p.addToStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_DOMAIN_NAME_COLLECTION_NAME, user.DomainName, user.Id, user.Id)
+    if oldUser != nil {
+        if oldUser.ShortName != user.ShortName {
+            p.delete(user.Id, _INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, oldUser.ShortName)
+        }
+        if oldUser.Name != user.Name {
+            p.removeFromStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_NAME_COLLECTION_NAME, oldUser.Name, user.Id, user.Id)
+        }
+        if oldUser.DomainName != user.DomainName {
+            p.removeFromStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_DOMAIN_NAME_COLLECTION_NAME, oldUser.DomainName, user.Id, user.Id)
+        }
+    }
+    return user, nil
 }
 
-func (p *InMemoryDataStore) DeleteConsumerAccount(user *dm.Consumer) (*dm.User, os.Error) {
-    return nil, nil
+func (p *InMemoryDataStore) DeleteConsumerAccount(user *dm.Consumer) (*dm.Consumer, os.Error) {
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        return user, dm.ERR_MUST_SPECIFY_ID
+    }
+    oldUserI, _ := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, user.Id)
+    var oldUser *dm.Consumer = nil
+    if oldUserI != nil {
+        oldUser = oldUserI.(*dm.Consumer)
+    }
+    if oldUser != nil {
+        p.delete(user.Id, _INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, user.Id)
+        p.delete(user.Id, _INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, oldUser.ShortName)
+        p.removeFromStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_NAME_COLLECTION_NAME, oldUser.Name, user.Id, user.Id)
+        p.removeFromStringMapCollection(user.Id, _INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_DOMAIN_NAME_COLLECTION_NAME, oldUser.DomainName, user.Id, user.Id)
+    }
+    return user, nil
 }
 
 
 func (p *InMemoryDataStore) CreateExternalUserAccount(user *dm.ExternalUser) (*dm.ExternalUser, os.Error) {
-    return nil, nil
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        user.Id = p.generateIdForAccount(_INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME)
+    }
+    if _, ok := p.retrieve(_INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, user.Id); ok {
+        return user, ba.ERR_ACCOUNT_ALREADY_EXISTS_WITH_SPECIFIED_ID
+    }
+    p.store(user.Id, _INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, user.Id, user)
+    p.addToStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_CONSUMER_ID_COLLECTION_NAME, user.ConsumerId, user.Id, user.Id)
+    p.addToStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_EXTERNAL_USER_ID_COLLECTION_NAME, user.ExternalUserId, user.Id, user.Id)
+    return user, nil
 }
 
 func (p *InMemoryDataStore) UpdateExternalUserAccount(user *dm.ExternalUser) (*dm.ExternalUser, os.Error) {
-    return nil, nil
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        return user, dm.ERR_MUST_SPECIFY_ID
+    }
+    oldUserI, _ := p.retrieve(_INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, user.Id)
+    var oldUser *dm.ExternalUser = nil
+    if oldUserI != nil {
+        oldUser = oldUserI.(*dm.ExternalUser)
+    }
+    p.store(user.Id, _INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, user.Id, user)
+    p.addToStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_CONSUMER_ID_COLLECTION_NAME, user.ConsumerId, user.Id, user.Id)
+    p.addToStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_EXTERNAL_USER_ID_COLLECTION_NAME, user.ExternalUserId, user.Id, user.Id)
+    if oldUser != nil {
+        if oldUser.ConsumerId != user.ConsumerId {
+            p.removeFromStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_CONSUMER_ID_COLLECTION_NAME, oldUser.ConsumerId, user.Id, user.Id)
+        }
+        if oldUser.ExternalUserId != user.ExternalUserId {
+            p.removeFromStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_EXTERNAL_USER_ID_COLLECTION_NAME, oldUser.ExternalUserId, user.Id, user.Id)
+        }
+    }
+    return user, nil
 }
 
 func (p *InMemoryDataStore) DeleteExternalUserAccount(user *dm.ExternalUser) (*dm.ExternalUser, os.Error) {
-    return nil, nil
+    if user == nil {
+        return nil, nil
+    }
+    if user.Id == "" {
+        return user, dm.ERR_MUST_SPECIFY_ID
+    }
+    oldUserI, _ := p.retrieve(_INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, user.Id)
+    var oldUser *dm.ExternalUser = nil
+    if oldUserI != nil {
+        oldUser = oldUserI.(*dm.ExternalUser)
+    }
+    if oldUser != nil {
+        p.delete(user.Id, _INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, user.Id)
+        p.removeFromStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_CONSUMER_ID_COLLECTION_NAME, oldUser.ConsumerId, user.Id, user.Id)
+        p.removeFromStringMapCollection(user.Id, _INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_EXTERNAL_USER_ID_COLLECTION_NAME, oldUser.ExternalUserId, user.Id, user.Id)
+    }
+    return user, nil
 }
 
 
 func (p *InMemoryDataStore) RetrieveUserAccountById(id string) (*dm.User, os.Error) {
+    user, _ := p.retrieve(_INMEMORY_USER_ACCOUNT_COLLECTION_NAME, id)
+    if user != nil {
+        return user.(*dm.User), nil
+    }
     return nil, nil
 }
 
 func (p *InMemoryDataStore) FindUserAccountByUsername(username string) (*dm.User, os.Error) {
-    return nil, nil
+    uidI, _ := p.retrieve(_INMEMORY_USER_ACCOUNT_ID_FOR_USERNAME_COLLECTION_NAME, username)
+    if uidI == nil {
+        return nil, nil
+    }
+    uid := uidI.(string)
+    if uid == "" {
+        return nil, nil
+    }
+    return p.RetrieveUserAccountById(uid)
 }
 
 func (p *InMemoryDataStore) FindUserAccountsByEmail(email string, next ba.NextToken, maxResults int) ([]*dm.User, ba.NextToken, os.Error) {
-    return nil, nil, nil
-}
-
-func (p *InMemoryDataStore) FindUserAccountsByPhoneNumber(phoneNumber string, next ba.NextToken, maxResults int) ([]*dm.User, ba.NextToken, os.Error) {
-    return nil, nil, nil
+    uidI, _ := p.retrieve(_INMEMORY_USER_ACCOUNT_ID_FOR_EMAIL_COLLECTION_NAME, email)
+    if uidI == nil {
+        return nil, nil, nil
+    }
+    uid := uidI.(string)
+    if uid == "" {
+        return nil, nil, nil
+    }
+    user, err := p.RetrieveUserAccountById(uid)
+    if user == nil {
+        return make([]*dm.User, 0), nil, err
+    }
+    return []*dm.User{user}, nil, err
 }
 
 
 func (p *InMemoryDataStore) RetrieveConsumerAccountById(id string) (*dm.Consumer, os.Error) {
+    user, _ := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_COLLECTION_NAME, id)
+    if user != nil {
+        return user.(*dm.Consumer), nil
+    }
     return nil, nil
 }
 
 func (p *InMemoryDataStore) FindConsumerAccountByShortName(shortName string) (*dm.Consumer, os.Error) {
-    return nil, nil
+    uidI, _ := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_ID_FOR_SHORTNAME_COLLECTION_NAME, shortName)
+    if uidI == nil {
+        return nil, nil
+    }
+    uid := uidI.(string)
+    if uid == "" {
+        return nil, nil
+    }
+    return p.RetrieveConsumerAccountById(uid)
 }
 
 func (p *InMemoryDataStore) FindConsumerAccountsByDomainName(domainName string, next ba.NextToken, maxResults int) ([]*dm.Consumer, ba.NextToken, os.Error) {
-    return nil, nil, nil
+    mI, _ := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_DOMAIN_NAME_COLLECTION_NAME, domainName)
+    if mI == nil {
+        return make([]*dm.Consumer, 0), nil, nil
+    }
+    m := mI.(map[string]string)
+    arr := make([]*dm.Consumer, len(m))
+    i := 0
+    for k := range m {
+        arr[i], _ = p.RetrieveConsumerAccountById(k)
+        i++
+    }
+    return arr, nil, nil
 }
 
 func (p *InMemoryDataStore) FindConsumerAccountsByName(name string, exact bool, next ba.NextToken, maxResults int) ([]*dm.Consumer, ba.NextToken, os.Error) {
-    return nil, nil, nil
+    // TODO handle non-exact name matches
+    mI, _ := p.retrieve(_INMEMORY_CONSUMER_ACCOUNT_IDS_FOR_NAME_COLLECTION_NAME, name)
+    if mI == nil {
+        return make([]*dm.Consumer, 0), nil, nil
+    }
+    m := mI.(map[string]string)
+    arr := make([]*dm.Consumer, len(m))
+    i := 0
+    for k := range m {
+        arr[i], _ = p.RetrieveConsumerAccountById(k)
+        i++
+    }
+    return arr, nil, nil
 }
 
 
 func (p *InMemoryDataStore) RetrieveExternalUserAccountById(id string) (*dm.ExternalUser, os.Error) {
+    user, _ := p.retrieve(_INMEMORY_EXTERNAL_USER_ACCOUNT_COLLECTION_NAME, id)
+    if user != nil {
+        return user.(*dm.ExternalUser), nil
+    }
     return nil, nil
 }
 
@@ -131,22 +355,35 @@ func (p *InMemoryDataStore) RetrieveExternalUserAccountByConsumerAndExternalUser
 }
 
 func (p *InMemoryDataStore) FindExternalUserAccountsByConsumerId(consumerId string, next ba.NextToken, maxResults int) ([]*dm.ExternalUser, ba.NextToken, os.Error) {
-    return nil, nil, nil
+    mI, _ := p.retrieve(_INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_CONSUMER_ID_COLLECTION_NAME, consumerId)
+    if mI == nil {
+        return make([]*dm.ExternalUser, 0), nil, nil
+    }
+    m := mI.(map[string]string)
+    arr := make([]*dm.ExternalUser, len(m))
+    i := 0
+    for k := range m {
+        arr[i], _ = p.RetrieveExternalUserAccountById(k)
+        i++
+    }
+    return arr, nil, nil
 }
 
 func (p *InMemoryDataStore) FindExternalUserAccountsByExternalUserId(externalUserId string, next ba.NextToken, maxResults int) ([]*dm.ExternalUser, ba.NextToken, os.Error) {
-    return nil, nil, nil
+    mI, _ := p.retrieve(_INMEMORY_EXTERNAL_ACCOUNT_IDS_FOR_EXTERNAL_USER_ID_COLLECTION_NAME, externalUserId)
+    if mI == nil {
+        return make([]*dm.ExternalUser, 0), nil, nil
+    }
+    m := mI.(map[string]string)
+    arr := make([]*dm.ExternalUser, len(m))
+    i := 0
+    for k := range m {
+        arr[i], _ = p.RetrieveExternalUserAccountById(k)
+        i++
+    }
+    return arr, nil, nil
 }
 
-
-func (p *InMemoryDataStore) AllowLoginByUserId(userId string) (bool, os.Error) {
-    return false, nil
-}
-
-func (p *InMemoryDataStore) DisableLogin(userId string) (os.Error) {
-    return nil
-}
-
-func (p *InMemoryDataStore) DisableLoginAt(userId string, at *time.Time) (os.Error) {
-    return nil
+func (p *InMemoryDataStore) BackendAccountsDataStore() (ba.DataStore) {
+    return p
 }
