e8f43cf4b9cebc66978943c84835ec4e234f3130
Aalok Shah <aalok@pomack.com>
2011-11-18 17:31:35 -0500

    Modify Authorization's DataStore interface

--- backend/authorization/service.go
+++ backend/authorization/service.go
@@ -9,16 +9,16 @@ import (
 type NextToken interface{}
 
 type DataStore interface {
-    SetSession(session *dm.Session) (*dm.Session, os.Error)
-    DeleteSession(sessionId string) (os.Error)
     RetrieveSession(sessionId string) (*dm.Session, os.Error)
+    StoreSession(session *dm.Session) (*dm.Session, os.Error)
+    DeleteSession(sessionId string) (os.Error)
     RetrieveSessionsForUserId(userId string, next NextToken, maxResults int) ([]*dm.Session, NextToken, os.Error)
     RetrieveSessionsForConsumerId(consumerId string, next NextToken, maxResults int) ([]*dm.Session, NextToken, os.Error)
     RetrieveSessionsForExternalUserId(externalUserId string, next NextToken, maxResults int) ([]*dm.Session, NextToken, os.Error)
     
-    SetAuthorizationToken(authToken *dm.AuthorizationToken) (*dm.AuthorizationToken, os.Error)
-    DeleteAuthorizationToken(authTokenId string) (os.Error)
     RetrieveAuthorizationToken(authTokenId string) (*dm.AuthorizationToken, os.Error)
+    StoreAuthorizationToken(authToken *dm.AuthorizationToken) (*dm.AuthorizationToken, os.Error)
+    DeleteAuthorizationToken(authTokenId string) (os.Error)
     RetrieveAuthorizationTokensForUserId(userId string, next NextToken, maxResults int) ([]*dm.AuthorizationToken, NextToken, os.Error)
     RetrieveAuthorizationTokensForConsumerId(consumerId string, next NextToken, maxResults int) ([]*dm.AuthorizationToken, NextToken, os.Error)
     RetrieveAuthorizationTokensForExternalUserId(externalUserId string, next NextToken, maxResults int) ([]*dm.AuthorizationToken, NextToken, os.Error)
